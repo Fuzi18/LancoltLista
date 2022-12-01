34b807f624e73f4d0e1d1b3aeb89433ee5fd20a3
Fuzi18 <fuzesi.t2003@gmail.com>
2022-12-01 10:32:42 +0100

    start

--- src/hu/petrik/lancoltlista/Main.java
+++ src/hu/petrik/lancoltlista/Main.java
@@ -1,4 +1,18 @@
 package hu.petrik.lancoltlista;
 
+import java.time.LocalDateTime;
+
 public class Main {
+    public static void main(String[] args) {
+        lancoltlista<Integer> list= new lancoltlista<>();
+        int elemekszama = 69;
+        int legkisebb = 1;
+        int legnagyobb = 420;
+        for (int i = 0; i < elemekszama; i++) {
+            int ertek = (int)(Math.random() * (legnagyobb - legkisebb + 1)) + legkisebb;
+            list.hozzaAd(ertek);
+
+        }
+
+    }
 }
--- src/hu/petrik/lancoltlista/lancoltlista.java
+++ src/hu/petrik/lancoltlista/lancoltlista.java
@@ -2,26 +2,53 @@ package hu.petrik.lancoltlista;
 
 public class lancoltlista <T>{
     private ListaElem elso;
+    private ListaElem utolso;
 
     public void hozzaAd(T ertek) {
         if (this.elso == null) {
-            elso = new ListaElem(ertek);
+            this.elso = new ListaElem(ertek);
+            this.utolso = new ListaElem(ertek);
 
-        }else {
-            elso.hozzaAd(ertek);
+        }else{
+            ListaElem ujUtolso = new ListaElem(ertek);
+            ujUtolso.setElozo(this.utolso);
+
         }
     }
 
     private class  ListaElem{
         private T ertek;
         private ListaElem kovetkezo;
+        private ListaElem elozo;
 
         public ListaElem(T ertek) {
             this.ertek = ertek;
             this.kovetkezo = null;
+            this.elozo= null;
+        }
+
+        public ListaElem getKovetkezo() {
+            return kovetkezo;
+        }
+
+        public void setKovetkezo(ListaElem kovetkezo) {
+            this.kovetkezo = kovetkezo;
+        }
+
+        public ListaElem getElozo() {
+            return elozo;
+        }
+
+        public void setElozo(ListaElem elozo) {
+            this.elozo = elozo;
         }
 
         public void hozzaAd(T ertek) {
+            if (this.kovetkezo == null){
+                this.kovetkezo = new ListaElem(ertek);
+            }else {
+                this.kovetkezo.hozzaAd(ertek);
+            }
         }
     }
 }
